6e2c2cbedf3a1b460ff64d8fe6e474ff85d68cfc
singhabhinav81 <singhabhinav81@gmail.com>
2017-10-27 13:30:15 +0530

    merge

---
+++ owncloud/config/config.php
@@ -0,0 +1,18 @@
+<?php
+$CONFIG = array (
+  'updatechecker' => false,
+  'instanceid' => 'ocm23uw7ff5i',
+  'passwordsalt' => 'FNJZANGoKz43tOLZIlkSKmy5kKO4T6',
+  'secret' => 'hp7enZj5SxAFGc3v13QhxGdhdhlwN+bZxp3uw2D35gFxIDa3',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.0.101',
+  ),
+  'datadirectory' => '/var/www/owncloud/data',
+  'overwrite.cli.url' => 'http://192.168.0.101/owncloud',
+  'dbtype' => 'sqlite3',
+  'version' => '9.1.6.2',
+  'logtimezone' => 'UTC',
+  'installed' => true,
+  'maintenance' => false,
+);
